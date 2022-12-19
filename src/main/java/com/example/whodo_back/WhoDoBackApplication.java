c22841a7233bb703360a0187ddcc35013790ace9
hajeu <s22072@gsm.hs.kr>
2022-12-19 09:48:36 +0900

    first commit

---
+++ src/main/java/com/example/whodo_back/WhoDoBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.whodo_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhoDoBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhoDoBackApplication.class, args);
+    }
+
+}
